4128165b42c01e933560f7a269880ae27b5a25e0
Laserkaier <andrxlop@gmail.com>
2026-03-06 15:58:44 +0300

    weqqweq

---
+++ Content.Shared/_Wega/Resomi/Abilities/WeaponNotBeInsabilityComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Shared.Resomi.NotInability;
+
+
+/// <summary>
+/// Даётся оружию, чтобы резоми
+/// стреляли из этого оружия нормально
+/// </summary>
+[RegisterComponent]
+public sealed partial class WeaponNotInabilityComponent : Component;
